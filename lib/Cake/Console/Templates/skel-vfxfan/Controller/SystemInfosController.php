50142937ebe265fafc86e5acb48a2b242b0e6332
Manuel Alducin <malducin@vfxfan.com>
2012-02-19 14:06:34 -0600

    Add first version of System Info system
    Added first version of System Info system. It displays some basic
    info about the site (project) for quick reference like CakePHP and
    PHP versions, available controllers, and URLs and paths for the
    site, images, javascript and CSS files.

---
+++ lib/Cake/Console/Templates/skel-vfxfan/Controller/SystemInfosController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * SystemInfos controller.
+ *
+ * SystemInfos actions. Display basic info of the system, like the
+ * CakePHP, PHP and MySQL versions and all present controllers.
+ *
+ * @author        Manuel Alducin
+ * @copyright     Copyright (c) 2009-2012, VFXfan (http://vfxfan.com)
+ * @link          http://vfxfan.com VFXfan
+ * @package       system_infos
+ * @subpackage    system_infos.controller
+ */
+App::uses('AppController', 'Controller');
+/**
+ * SystemInfos Controller
+ *
+ * @property SystemInfo $SystemInfo
+ */
+class SystemInfosController extends AppController {
+/**
+ * Models to use
+ *
+ * @var array
+ */
+	public $uses = array();
+
+/**
+ * admin_index method
+ *
+ * @return void
+ */
+	public function admin_index() {
+		$this->layout = 'default_admin';
+
+		// get system variables
+		$cakephpVersion = Configure::version();
+		$phpVersion = phpversion();
+		$controllersPresent = str_replace('Controller', '', App::objects('Controller'));
+
+		$this->set('title_for_layout', 'System Info');
+		$this->set(compact('cakephpVersion', 'phpVersion', 'controllersPresent'));
+	}
+
+}
