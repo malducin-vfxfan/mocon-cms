11180f9d34e32f8b65780a794f383857706bf5ec
Manuel Alducin <malducin@vfxfan.com>
2012-04-05 09:03:48 -0500

    Fix ambiguous id field in afterSave and beforeDelete methods
    Fixed the ambiguous id field in afterSave and beforeDelete methods by
    using the model name. Also added a recusive = -1 option to the find
    operations.

--- lib/Cake/Console/Templates/skel-vfxfan/Model/Post.php
+++ lib/Cake/Console/Templates/skel-vfxfan/Model/Post.php
@@ -176,7 +176,7 @@ class Post extends AppModel {
 	public function afterSave($created) {
 		// check to see if a year folder exists and if not, create one
 		if ($created) {
-			$post = $this->find('first', array('conditions' => array('id' => $this->id), 'recursive' => -1));
+			$post = $this->find('first', array('conditions' => array('Post.id' => $this->id), 'recursive' => -1));
 			if (!is_file(IMAGES.'posts'.DS.$post['Post']['year'])) {
 				mkdir(IMAGES.'posts'.DS.$post['Post']['year']);
 			}
@@ -193,7 +193,7 @@ class Post extends AppModel {
  * @return boolean
  */
 	public function beforeDelete($cascade) {
-		$post = $this->find('first', array('conditions' => array('id' => $this->id)));
+		$post = $this->find('first', array('conditions' => array('Post.id' => $this->id), 'recursive' => -1));
 		$directory = IMAGES.'posts'.DS.$post['Post']['year'];
 		$filebasename = sprintf("%010d", $this->id);
 
